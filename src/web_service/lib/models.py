8b8a63a0e72bca1a229729ea3c398484b13da103
Guillaume-ds <guillaume.de-surville@hec.edu>
2023-10-23 15:38:13 +0200

    web_app update

--- src/web_service/lib/models.py
+++ src/web_service/lib/models.py
@@ -1,1 +1,16 @@
-# Pydantic models for the web service
+from pydantic import BaseModel
+
+
+class InputData(BaseModel):
+    Sex: int
+    Length: int
+    Diameter: int
+    Height: int
+    Whole_weight: int
+    Shucked_weight: int
+    Viscera_weight: int
+    Shell_weight: int
+
+
+class AbaloneAge(BaseModel):
+    age: float
