3bbe9176a11c60bad9c1ba074329d55ea75f6a7c
Guillaume-ds <guillaume.de-surville@hec.edu>
2023-10-23 16:12:54 +0200

    modifying post request

--- src/web_service/app_config.py
+++ src/web_service/app_config.py
@@ -1,7 +1,7 @@
 # MODELS
 MODEL_VERSION = "0.0.1"
-PATH_TO_PREPROCESSOR = f"local_models/dv__v{MODEL_VERSION}.pkl"
-PATH_TO_MODEL = f"local_models/model__v{MODEL_VERSION}.pkl"
+
+PATH_TO_MODEL = f"../local_models/model__v{MODEL_VERSION}.pkl"
 CATEGORICAL_VARS = [
     "Sex",
     "Length",
--- src/web_service/lib/modelling.py
+++ src/web_service/lib/modelling.py
@@ -1,35 +1,36 @@
-from typing import List
+import json
 
 import numpy as np
 import pandas as pd
 from lib.models import InputData
-from lib.preprocessing import CATEGORICAL_COLS, encode_categorical_cols
 from loguru import logger
 from sklearn.base import BaseEstimator
-from sklearn.feature_extraction import DictVectorizer
 
 
-def run_inference(
-    input_data: List[InputData], dv: DictVectorizer, model: BaseEstimator
-) -> np.ndarray:
+def run_inference(input_data: InputData, model: BaseEstimator) -> np.ndarray:
     """Run inference on a list of input data.
 
     Args:
         payload (dict): the data point to run inference on.
-        dv (DictVectorizer): the fitted DictVectorizer object.
         model (BaseEstimator): the fitted model object.
 
     Returns:
         np.ndarray: the predicted trip durations in minutes.
 
     Example payload:
-        {'PULocationID': 264, 'DOLocationID': 264, 'passenger_count': 1}
+        json of Inputs
     """
     logger.info(f"Running inference on:\n{input_data}")
     df = pd.DataFrame([x.dict() for x in input_data])
-    df = encode_categorical_cols(df)
-    dicts = df[CATEGORICAL_COLS].to_dict(orient="records")
-    X = dv.transform(dicts)
+    X = df.rename(
+        columns={
+            "Whole_weight": "Whole weight",
+            "Shucked_weight": "Shucked weight",
+            "Viscera_weight": "Viscera weight",
+            "Shell_weight": "Shell weight",
+        }
+    )
     y = model.predict(X)
-    logger.info(f"Predicted trip durations:\n{y}")
+    logger.info(f"Predicted abalone age:\n{y}")
+
     return y
