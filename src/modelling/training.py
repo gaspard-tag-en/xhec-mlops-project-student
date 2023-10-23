3682ba2ec1bd519f448c0f5de03ecb4bbcb76d74
Matthieu Haguenauer <mathaguen@gmail.com>
2023-10-23 19:27:12 +0200

    CI fixed

--- src/modelling/training.py
+++ src/modelling/training.py
@@ -5,7 +5,5 @@ from sklearn.pipeline import Pipeline
 
 
 @flow
-def train_model(
-    pipeline: Pipeline, x_train: pd.DataFrame, y_train: pd.DataFrame
-):
+def train_model(pipeline: Pipeline, x_train: pd.DataFrame, y_train: pd.DataFrame):
     return pipeline.fit(x_train, y_train)
